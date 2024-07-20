38136e807d7c7634ea10898c03e85da032e5bbb7
BilalKhoffash <goldenpotatoes.1204@gmail.com>
2024-07-21 02:17:52 +0400

    Added playGame function, nested playRound in it

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -20,24 +17,40 @@ function getHumanChoice() {
     return prompt("Which do you choose to pick? Rock, Paper or Scissors?").toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) { 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    if (humanChoice === computerChoice) {
-        console.log("Tie! Nobody wins")
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") || 
-        (humanChoice === "scissors" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "rock")
-    ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1;
+    function playRound(humanChoice, computerChoice) { 
+
+        if (humanChoice === computerChoice) {
+            console.log("Tie! Nobody wins")
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") || 
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "rock")
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore += 1;
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore += 1;
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (computerScore > humanScore) {
+        console.log("The computer won! Better luck next time..");
+    } else if (computerScore < humanScore) {
+        console.log("You won!!");
     } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore += 1;
+        console.log("It was a boring draw!");
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
