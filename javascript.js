0e58fdf6585eeae3173490a82d9bc32f7322c601
BilalKhoffash <goldenpotatoes.1204@gmail.com>
2024-07-23 02:01:26 +0400

    parts 1 - 3

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -13,37 +16,17 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Which do you choose to pick? Rock, Paper or Scissors?").toLowerCase();
-}
+const result = document.querySelector(".result")
+const choice = document.querySelector("#choices");
+
+choice.addEventListener('click', (e) => {
+    let target = e.target;
+    let computerSelection = getComputerChoice();
+
+    playRound(target.id, computerSelection);
+});
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) { 
-
-        if (humanChoice === computerChoice) {
-            console.log("Tie! Nobody wins");
-        } else if (
-            (humanChoice === "rock" && computerChoice === "scissors") || 
-            (humanChoice === "scissors" && computerChoice === "paper") ||
-            (humanChoice === "paper" && computerChoice === "rock")
-        ) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore += 1;
-        } else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore += 1;
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
     if (computerScore > humanScore) {
         console.log("The computer won! Better luck next time..");
     } else if (computerScore < humanScore) {
@@ -53,4 +36,19 @@ function playGame() {
     }
 }
 
-playGame();
+function playRound(humanChoice, computerChoice) { 
+
+    if (humanChoice === computerChoice) {
+        result.textContent = "Tie! Nobody wins";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") || 
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        result.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+        humanScore += 1;
+    } else {
+        result.textContent = `You lose! ${computerChoice} beats ${humanChoice}`
+        computerScore += 1;
+    }
+}
